beb61530328b2ca3aecaa113bff924ca815679d3
Hugo <hugo.ribaud@epitech.eu>
2021-11-09 15:05:59 +0100

    marker fixed

--- pages/map.js
+++ pages/map.js
@@ -1,50 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, Dimensions, Platform, Text} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Callout, Circle, Marker} from 'react-native-maps';
 
 const Map = () => {
-  const [location, setLocation] = useState();
-  const [errorMsg, setErrorMsg] = useState();
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
 
   return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
           initialRegion={{
-            latitude: 43.2701,
-            longitude: 5.3925,
-            latitudeDelta: 0.0122,
-            longitudeDelta: 0.0021,
+            latitude: 43.2700,
+            longitude: 5.3955,
+            latitudeDelta: 0.0111,
+            longitudeDelta: 0.0016,
           }}
-        />
+        >
         <Marker
-          coordinate={{latitude: 43.2701, longitude: 5.3925}}
-          title={"title"}
-          description={"description"}
-        />
-        <Text
-          style={styles.paragraph}>{text}
-        </Text>
+          coordinate={{latitude: 43.2700, longitude: 5.3955}}
+          draggable={false}
+        >
+        <Callout>
+          <Text color='blue'>My Position</Text>
+
+        </Callout>
+        </Marker>
+        <Circle
+          center={{latitude: 43.2700, longitude: 5.3955,}}
+          radius={200}/>
+        </MapView>
       </View>
   );
 };
